c21fb191818c593a027053c6641ee6e5c9005ee1
Kazutaka KAMIYA <kzkamiya@gmail.com>
2018-07-05 00:56:51 +0900

    Added FSharp.Data package in iOS.

--- SampleListView/MainPage.xaml.fs
+++ SampleListView/MainPage.xaml.fs
@@ -7,9 +7,9 @@ open Xamarin.Forms.Xaml
 open FSharp.Data
 open Deedle
 
-type Weather = JsonProvider<"https://api.openweathermap.org/data/2.5/weather?units=metric&q=Prague&appid=xxxxxx">
+type Weather = JsonProvider<"https://api.openweathermap.org/data/2.5/weather?units=metric&q=Prague&appid=cbaaf9e7cf14679936b009da4c32af31">
 
-type CurrentTemp(place : string, temp: int) = 
+type CurrentTemp(place : string, temp: decimal) = 
     member x.Place = place
     member x.Temp = temp
 
@@ -34,11 +34,10 @@ type MainPage() =
 
     let getCurrentTemp place =
       try
-//        let w = Weather.Load("https://api.openweathermap.org/data/2.5/weather?units=metric&q=Prague&appid=xxxxxxx")
-//        w.Main.Temp
-          10
+        let w = Weather.Load("https://api.openweathermap.org/data/2.5/weather?units=metric&q=" + place + "&appid=cbaaf9e7cf14679936b009da4c32af31")
+        w.Main.Temp
       with
-        | :? System.Net.WebException -> int(0)
+        | :? System.Net.WebException -> decimal(0)
 
     let place = "Cambridge,UK"
     let temp = getCurrentTemp place
